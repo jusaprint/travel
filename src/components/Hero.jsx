3a95865266616f6c28f4b2be0e86eaa9c49ab3cb
jusaprint <jusa2001@gmail.com>
2025-05-19 19:38:01 +0200

    Make hero images full width

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -195,11 +195,11 @@ const Hero = () => {
         <div className="absolute inset-0 z-0">
           <div
             className="absolute inset-0 w-full h-full bg-center bg-no-repeat bg-cover"
-            style={{ backgroundImage: `url('/kudosimheroimage.jpeg')` }}
+            style={{ backgroundImage: `url('${heroSettings.background_image}')` }}
           />
           <link
             rel="preload"
-            href="/kudosimheroimage.jpeg"
+            href={heroSettings.background_image}
             as="image"
             fetchPriority="high"
           />
@@ -289,7 +289,7 @@ const Hero = () => {
                   <motion.img
                     src={heroSettings.phone_image}
                     alt="eSIM Device"
-                    className="w-full max-w-[224px] mx-auto"
+                    className="w-full mx-auto"
                     width="224"
                     height="448"
                     loading="eager"
